b6e7ecf6d130f068fd356c362fd9a5d233766b97
Rico Mitchell <ricomitch@icloud.com>
2019-10-15 02:58:30 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,10 @@
 def square_array(array)
-square_array=[]  
-array.each do |element|
+square_array = []  
+ array.each do |element|
   
-squared_array.push(element**2)
-p element**2
+ squared_array.push(element**2)
+ p element**2
 end
 squared_array
 end
+
